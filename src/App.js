a02b535e909b7ee09810f0a17c91fc8b29b1ba30
William Holz <holz.willian@gmail.com>
2024-07-24 22:41:58 -0300

    m

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React <W>ORLD</W>
+          Learn React W
         </a>
       </header>
     </div>
